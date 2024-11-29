64c26c2d49241ca4463c99b085fb797164e40f8c
Leonard Richertz <leo.richertz@gmx.de>
2024-11-29 11:13:10 +0100

    added weather api functionality

--- app/Http/Controllers/WeatherDataController.php
+++ app/Http/Controllers/WeatherDataController.php
@@ -3,30 +3,48 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use GuzzleHttp\Client;
 
 class WeatherDataController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $client = new \GuzzleHttp\Client();
-        $response = $client->request('GET', 'https://api.openweathermap.org/data/2.5/weather', [
-            'headers' => [
-                'Authorization' => 'Bearer YOUR_API_TOKEN',
-            ],
-            'query' => [
-                'q' => 'London',
-                'appid' => 'YOUR_API_KEY'
-            ]
-        ]);
+        $lat = $request->input('lat');
+        $lon = $request->input('lon');
 
-        $weatherData = json_decode($response->getBody(), true);
-        // Fetch weather data from the openWeather API and send back to the user.
-        // Use API Token from openWeather API to fetch the data.
-        $data = [
-            'status' => 200,
-            'message' => 'Weather data fetched successfully',
-        ];
-
-        return response()->json($data, 200);
+        if (!$lat || !$lon) {
+            $data = [
+                'status' => 400,
+                'message' => 'Latitude and Longitude are required',
+            ];
+            return response()->json($data, 400);
+        }
+        try{
+            $client = new Client();
+            $response = $client->request('GET', env('OPENWEATHER_API_URL'), [
+                'query' => [
+                    'lat' => $lat,
+                    'lon' => -$lon,
+                    'exclude' => 'hourly,daily',
+                    'appid' => env('OPENWEATHER_API_KEY'),
+                ],
+                'verify' => false, // Disbale SSL verification. Not recommended..
+            ]);
+            $weatherData = json_decode($response->getBody(), true);
+        
+            $data = [
+                'status' => 200,
+                'message' => 'Weather data fetched successfully',
+                'data' => $weatherData,
+            ];
+            return response()->json($data, 200);
+        } catch (\Exception $e) {
+            $data = [
+                'status' => 500,
+                'message' => 'Failed to fetch weather data',
+                'error' => $e->getMessage(),
+            ];
+            return response()->json($data, 500);
+        }      
     }
 }
